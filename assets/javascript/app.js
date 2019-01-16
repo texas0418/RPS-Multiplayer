ad3ec306bc94bc4cc0731213051d00bf3e6be9af
texas0418 <simonshih@hotmail.com>
2019-01-16 14:50:25 -0600

    html and pseudo-code complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,57 +1,64 @@
-// Creates an array that lists out all of the options (Rock, Paper, Scissors, Lizard, Spock).
-var computerChoices = [
-"r",
-"p",
-"s",
-"l",
-"sp"
-];
-      
-// Creating variables to hold the number of wins, losses, and ties. They start at 0.
-var wins = 0;
-var losses = 0;
-var ties = 0;
-
-// Create variables that hold references to the places in the HTML where we want to display things.
-var directionsText = document.getElementById("directions-text");
-var userChoiceText = document.getElementById("userchoice-text");
-var computerChoiceText = document.getElementById("computerchoice-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var tiesText = document.getElementById("ties-text");
-
-// This function is run whenever the user presses a key.
-document.onkeyup = function(event) {
-
-  // Determines which key was pressed.
-  var userGuess = event.key;
-
-  // Randomly chooses a choice from the options array. This is the Computer's guess.
-  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-  // Reworked our code from last step to use "else if" instead of lots of if statements.
-
-  // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-  if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
-
-    if ((userGuess === "r" && computerGuess === "s") ||
-      (userGuess === "s" && computerGuess === "p") || 
-      (userGuess === "p" && computerGuess === "r")) {
-      wins++;
-    } else if (userGuess === computerGuess) {
-      ties++;
-    } else {
-      losses++;
-    }
-
-    // Hide the directions
-    directionsText.textContent = "";
-
-    // Display the user and computer guesses, and wins/losses/ties.
-    userChoiceText.textContent = "You chose: " + userGuess;
-    computerChoiceText.textContent = "The computer chose: " + computerGuess;
-    winsText.textContent = "wins: " + wins;
-    lossesText.textContent = "losses: " + losses;
-    tiesText.textContent = "ties: " + ties;
-  }
+var config = {
+  apiKey: "AIzaSyAslaCndOsIfhgdKULmFAOBeIsozPRylKw",
+  authDomain: "firstclassproject-36dd6.firebaseapp.com",
+  databaseURL: "https://firstclassproject-36dd6.firebaseio.com",
+  projectId: "firstclassproject-36dd6",
+  storageBucket: "firstclassproject-36dd6.appspot.com",
+  messagingSenderId: "160542958584"
 };
+
+  firebase.initializeApp(config);
+
+// Assign the reference to the database to a variable named 'database'
+var database = firebase.database();
+
+// Initial values
+// Stores objects for players
+var player1 = null;
+var player2 = null;
+
+// Stores players' names
+
+
+// Stores player's name in browser
+
+
+// Stores player's choice after button click
+
+
+// Tracks whos turn
+
+
+// At the initial load and subsequent value changes, get a snapshot of the stored data.
+// This function allows you to update your page in real-time when the firebase database changes.
+database.ref().on("value", function(snapshot) {
+
+
+// If Firebase has a player 1 and player 2 stored (first case)
+
+
+// Set the variables for player 1/player 2 equal to the stored values in firebase.
+
+
+// Change the HTML to reflect the stored values
+
+
+// When both players join, set player 1 as first turn - increase font size and change color (faded when not turn)
+
+
+// Grab Player 1's Choice
+
+
+// Grab Player 2's Choice
+
+
+// Rock Paper Scissors Lizard Spock Comparison
+
+
+// Highlight option that is the one based on who picked what. If tie, nothing is highlighted and Tie is displayed on both player's Wins/Losses
+
+
+// Change back to player's 1 turn
+
+
+// Chat box
